4c02128a349d0095d07f090e87e2eb27595ef34c
Giovana Albanês <albanesgiovana@gmail.com>
2025-07-14 20:03:40 -0300

    fix: corrige configuração de entidades e mapeamento JPA

--- cadastro-livros/src/main/java/com/portfolio/cadastro_livros/CadastroLivrosApplication.java
+++ cadastro-livros/src/main/java/com/portfolio/cadastro_livros/CadastroLivrosApplication.java
@@ -2,12 +2,13 @@ package com.portfolio.cadastro_livros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.portfolio.cadastro_livros.infrastructure.entitys")
 public class CadastroLivrosApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CadastroLivrosApplication.class, args);
 	}
-
 }
+
