49ad38fd17f0f01924a4c2f6d2ac684e9d3af918
Giovana Albanês <albanesgiovana@gmail.com>
2025-07-14 16:33:27 -0300

    feat: definição da arquitetura

---
+++ cadastro-livros/src/main/java/com/portfolio/cadastro_livros/CadastroLivrosApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.cadastro_livros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroLivrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroLivrosApplication.class, args);
+	}
+
+}
---
+++ cadastro-livros/src/test/java/com/portfolio/cadastro_livros/CadastroLivrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.cadastro_livros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroLivrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
