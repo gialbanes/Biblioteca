a57b59fce7caeb19f5ea59b457b4954e956c391c
Giovana Albanês <albanesgiovana@gmail.com>
2025-07-14 20:02:37 -0300

    feat: criação do controller do book

---
+++ cadastro-livros/src/main/java/com/portfolio/cadastro_livros/controller/BookController.java
@@ -0,0 +1,37 @@
+package com.portfolio.cadastro_livros.controller;
+
+import com.portfolio.cadastro_livros.business.BookService;
+import com.portfolio.cadastro_livros.infrastructure.entitys.Book;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/book")
+@RequiredArgsConstructor
+public class BookController {
+    private final BookService bookService;
+
+    @PostMapping //para gravar
+    public ResponseEntity<Void> saveBook(@RequestBody Book book){
+        bookService.saveBook(book);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Book> findBookById(@RequestParam Long id){
+        return ResponseEntity.ok(bookService.findBookById(id));
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteBookById(@RequestParam Long id){
+        bookService.deleteBookById(id);
+        return ResponseEntity.ok().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateBookById(@RequestParam Long id, @RequestBody Book book){
+        bookService.saveBook(book);
+        return ResponseEntity.ok().build();
+    }
+}
