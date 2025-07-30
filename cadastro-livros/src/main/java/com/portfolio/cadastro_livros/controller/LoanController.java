e9443d81063e2147f7937df294abf3dc59c2c8ad
Giovana Albanês <albanesgiovana@gmail.com>
2025-07-30 16:42:33 -0300

    feat: métodos CRUD do loan

--- cadastro-livros/src/main/java/com/portfolio/cadastro_livros/controller/LoanController.java
+++ cadastro-livros/src/main/java/com/portfolio/cadastro_livros/controller/LoanController.java
@@ -1,4 +1,38 @@
 package com.portfolio.cadastro_livros.controller;
 
+import com.portfolio.cadastro_livros.business.LoanService;
+import com.portfolio.cadastro_livros.infrastructure.entitys.Loan;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/loan")
+@RequiredArgsConstructor
 public class LoanController {
+    private final LoanService loanService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveLoan(@RequestBody Loan loan){
+        loanService.saveLoan(loan);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Loan> findLoanById(@RequestParam Long id){
+        loanService.findLoanById(id);
+        return ResponseEntity.ok(loanService.findLoanById(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateLoan(@RequestParam Long id, @RequestBody Loan loan){
+        loanService.updateLoan(id, loan);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteLoan(@RequestParam Long id){
+        loanService.deleteLoan(id);
+        return ResponseEntity.ok().build();
+    }
 }
